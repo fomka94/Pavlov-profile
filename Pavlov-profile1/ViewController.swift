072dbdb1df1bf1ff26ee61a761ac649dae27708f
fomka94 <fom-aina@mail.ru>
2022-05-14 22:22:53 +0900

    Initial Commit

---
+++ Pavlov-profile1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Pavlov-profile1
+//
+//  Created by Фомина Айина Валерьевна on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func userStatusBottom(_ sender: Any) {
+    }
+    @IBAction func profileEditBottom(_ sender: Any) {
+    }
+    @IBAction func profileHistoryBottom(_ sender: Any) {
+    }
+    @IBAction func profileNoteBottom(_ sender: Any) {
+    }
+    @IBAction func profilePhotoBottom(_ sender: Any) {
+    }
+    @IBAction func profileClipBottom(_ sender: Any) {
+    }
+    @IBAction func workplaceBottom(_ sender: Any) {
+    }
+    @IBOutlet weak var receiveGiftBottom: UIStackView!
+    
+    @IBAction func infoBottom(_ sender: Any) {
+    }
+    
+}
+
