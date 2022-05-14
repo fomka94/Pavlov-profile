155830da2ad84ab0340f2048a25a3019564afcee
fomka94 <fom-aina@mail.ru>
2022-05-14 22:22:53 +0900

    Initial Commit

---
+++ Pavlov-profile1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pavlov-profile1
+//
+//  Created by Фомина Айина Валерьевна on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
