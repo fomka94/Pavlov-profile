4601f153d54c656016ff14684188eb0ea12d4dde
fomka94 <fom-aina@mail.ru>
2022-05-23 19:45:31 +0900

    push finish profile

--- Pavlov-profile1/ViewController.swift
+++ Pavlov-profile1/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// profile
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
