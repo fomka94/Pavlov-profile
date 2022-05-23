2f94b413f4b7981a89f71a5af7e14f72d4443979
fomka94 <fom-aina@mail.ru>
2022-05-23 19:26:32 +0900

    test commit2

--- Pavlov-profile1/ViewController.swift
+++ Pavlov-profile1/ViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// test comment
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func userStatusBottom(_ sender: Any) {
     }
+    
+    
     
     @IBAction func profileEditBottom(_ sender: Any) {
     }
